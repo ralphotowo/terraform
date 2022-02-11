661a3bef0df0469e92f240e61f094a0e1fa5cef6
Ralph Otowo <ralph@otowo.me>
2022-02-11 23:46:56 +0100

    test

--- cloudsql-postgres/provider.tf
+++ cloudsql-postgres/provider.tf
@@ -7,9 +7,8 @@ terraform {
   }
 }
 
-provider "google" {
-#  credentials = "${file("account.json")}"
-
-  project = var.project_id
-  region = var.region
-}
+# provider "google" {
+#   credentials = "${file("account.json")}"
+#   project = var.project_id
+#   region = var.region
+# }
