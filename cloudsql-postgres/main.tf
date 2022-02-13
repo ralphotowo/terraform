95a750d1f2253e666f07b9ca37f874e895e6a146
Ralph Otowo <ralph@otowo.me>
2022-02-13 11:46:48 +0100

    Clean up

--- cloudsql-postgres/main.tf
+++ cloudsql-postgres/main.tf
@@ -1,15 +1,31 @@
 resource "google_sql_database_instance" "master" {
-  name             = "master-instance"
-  database_version = "POSTGRES_11"
-  region           = "${var.region}"
+  name                  = "master-instance"
+  database_version      = "POSTGRES_11"
+  region                = "${var.region}"
+  root_password         = var.root_passwd
+  deletion_protection   = false
+  availability_policy   = "REGIONAL"
 
   settings {
-    tier = "db-n1-standard-4"
-    root_password = var.root_passwd
-    deletion_protection = false
-    availability_policy = "REGIONAL"
-    disk_size = "200"
-    disk_type = "PD_SSD"
+    tier        = "db-n1-standard-4"
+    disk_size   = "200"
+    disk_type   = "PD_SSD"
     }
 
 }
+
+# resource "google_sql_database_instance" "master" {
+#   name             = "master-instance"
+#   database_version = "POSTGRES_11"
+#   region           = "${var.region}"
+
+#   settings {
+#     tier = "db-n1-standard-4"
+#     root_password = var.root_passwd
+#     deletion_protection = false
+#     availability_policy = "REGIONAL"
+#     disk_size = "200"
+#     disk_type = "PD_SSD"
+#     }
+
+# }
