7c63118df5188b26a44205f2b584181148131f87
Ralph Otowo <ralph@otowo.me>
2022-02-13 16:05:49 +0100

    DB Name

--- cloudsql-postgres/main.tf
+++ cloudsql-postgres/main.tf
@@ -15,7 +15,7 @@ resource "google_sql_database_instance" "datascience-apps-db" {
 
     ip_configuration {
       ipv4_enabled      = true
-
+    
       authorized_networks {
           name          = "ralph"
           value         = "102.220.190.21/32"
--- cloudsql-postgres/terraform.auto.tfvars
+++ cloudsql-postgres/terraform.auto.tfvars
@@ -4,6 +4,6 @@ zone = "europe-west2-a"
 
 db_tier = "db-custom-2-13312"
 db_user = "gcp-usr"
-db_name = "datascience-apps-1"
+db_name = "datascience-db"
 db_version = "POSTGRES_11"
 db_root_passwd = "ooHNdF1O}CPQ#Oz).KOA"
