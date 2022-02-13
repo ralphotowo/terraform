f3174864c1959937d994a19ff2ac01e4823adab7
Ralph Otowo <ralph@otowo.me>
2022-02-13 11:22:18 +0100

    Fresh start

--- cloudsql-postgres/main.tf
+++ cloudsql-postgres/main.tf
@@ -1,26 +1,25 @@
 resource "google_sql_database_instance" "master" {
   name             = "master-instance"
   database_version = "POSTGRES_11"
-  region           = var.region
+  region           = "us-central1"
 
   settings {
-    project = var.project
-    tier = "db-n1-standard-4"
-    root_password = var.root_passwd
-    deletion_protection = false
-    availability_policy = "REGIONAL"
-    disk_size = "200"
-    disk_type = "PD_SSD"
-
-    settings.backup_configuration {
-        backup_retention_settings
-    }
-
-    settings.ip_configuration {
-        ipv4_enabled = true
-    }
-
-    
-
+    tier = "db-f1-micro"
   }
 }
+
+# resource "google_sql_database_instance" "master" {
+#   name             = "master-instance"
+#   database_version = "POSTGRES_11"
+#   region           = "${var.region}"
+
+#   settings {
+#     tier = "db-n1-standard-4"
+#     root_password = var.root_passwd
+#     deletion_protection = false
+#     availability_policy = "REGIONAL"
+#     disk_size = "200"
+#     disk_type = "PD_SSD"
+#     }
+
+# }
