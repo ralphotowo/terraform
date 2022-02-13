58c5468a748d439ff5bc1f916611ef75763afcce
Ralph Otowo <ralph@otowo.me>
2022-02-13 11:35:05 +0100

    DB Configuration

--- cloudsql-postgres/main.tf
+++ cloudsql-postgres/main.tf
@@ -1,25 +1,15 @@
 resource "google_sql_database_instance" "master" {
   name             = "master-instance"
   database_version = "POSTGRES_11"
-  region           = "us-central1"
+  region           = "${var.region}"
 
   settings {
-    tier = "db-f1-micro"
-  }
+    tier = "db-n1-standard-4"
+    root_password = var.root_passwd
+    deletion_protection = false
+    availability_policy = "REGIONAL"
+    disk_size = "200"
+    disk_type = "PD_SSD"
+    }
+
 }
-
-# resource "google_sql_database_instance" "master" {
-#   name             = "master-instance"
-#   database_version = "POSTGRES_11"
-#   region           = "${var.region}"
-
-#   settings {
-#     tier = "db-n1-standard-4"
-#     root_password = var.root_passwd
-#     deletion_protection = false
-#     availability_policy = "REGIONAL"
-#     disk_size = "200"
-#     disk_type = "PD_SSD"
-#     }
-
-# }
