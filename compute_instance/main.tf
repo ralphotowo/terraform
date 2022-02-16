38c1879469499ee71edac83573a2de5d59052867
Ralph Otowo <ralph@otowo.me>
2022-02-16 17:18:34 +0100

    Firewall

--- compute_instance/main.tf
+++ compute_instance/main.tf
@@ -25,3 +25,21 @@ resource "google_compute_instance" "default" {
   }
 
 }
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+}
