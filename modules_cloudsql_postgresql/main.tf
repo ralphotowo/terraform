26884e15684b84c52e35c6fc7770f17961901d48
Ralph Otowo <ralph@otowo.me>
2022-03-01 13:34:39 +0100

    Private network 9

--- modules_cloudsql_postgresql/main.tf
+++ modules_cloudsql_postgresql/main.tf
@@ -29,7 +29,7 @@ module "datascience-sql-db" {
 }
 
 module "private-service-access" {
-  source      = "../../modules/private_service_access"
+  source      = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
   project_id  = var.project_id
   vpc_network = "default"
 }
---
+++ sql-db/main.tf
@@ -0,0 +1,4 @@
+module "sql-db" {
+  source  = "GoogleCloudPlatform/sql-db/google"
+  version = "9.0.0"
+}
