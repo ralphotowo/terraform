320d45cc1c82353fb1a5cbf9329ea5048abc5d25
Ralph Otowo <ralph@otowo.me>
2022-03-03 13:46:44 +0100

    Removed network from data block

--- modules_cloudsql_postgresql/main.tf
+++ modules_cloudsql_postgresql/main.tf
@@ -2,7 +2,7 @@ data "google_compute_global_address" "google-managed-services-range" {
 #  source      = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
   name        = "google_compute_global_address"
   project  = var.project_id
-  network = var.vpc_network
+#  network = var.vpc_network
 }
 
 module "datascience-sql-db" {
